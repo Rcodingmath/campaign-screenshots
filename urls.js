2bd46ee29a50862b7d0f287fd2026760f8bda620
Rcodingmath <76174700+Rcodingmath@users.noreply.github.com>
2026-04-06 17:23:06 +0530

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,6 +1,2 @@
-module.exports = [
-  'https://www.magnumicecream.com/smc/magnum/best-precious-2025.html',
-  'https://www.calve.com.tr/smc/calve/calve-turkey.html',
-  'https://www.close-up.com/smc/closeup/closeup-turkey.html',
-  
-];
+//This file is no longer needed
+module.exports = [];
